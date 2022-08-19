be0f6bed02421fcdf29f4405800ed1315eca687a
Konrad Pawelec <konrad.pawelec10@gmail.com>
2022-08-19 18:30:56 +0200

    New section portfolio

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import requests
 
 app = Flask(__name__)
+Bootstrap(app)
+
+r = requests.get("https://api.github.com/users/radk0n9/repos")
+github_json = r.json()
+for p in github_json:
+    print(p["created_at"].split("T")[0])
+# first_one = github_json[0]
+# print(first_one["name"])
 
 
 @app.route("/")
 @app.route("/index.html")
 def home():
-    return render_template("index.html")
+    how_many_items_portfolio = github_json
+    return render_template("index.html", work_contents=how_many_items_portfolio)
 
 
 if __name__ == "__main__":
