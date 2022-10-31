a9f52c492b64274882051903040e759c927bfd2f
Konrad Pawelec <konrad.pawelec10@gmail.com>
2022-10-31 19:05:54 +0100

    Added gunicorn to host website in heroku

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ Bootstrap(app)
 
 r = requests.get("https://api.github.com/users/radk0n9/repos")
 github_json = r.json()
+
+
 # for p in github_json:
 #     print(p["created_at"].split("T")[0])
 # first_one = github_json[0]
 # print(first_one["name"])
 
 
-@app.route("/")
-@app.route("/index.html")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index.html", methods=["GET", "POST"])
 def home():
     how_many_items_portfolio = github_json
-    return render_template("index.html", work_contents=how_many_items_portfolio)
-
-
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
     if request.method == "POST":
         data = request.form
         print(data)
+    return render_template("index.html", work_contents=how_many_items_portfolio)
 
 
 if __name__ == "__main__":
